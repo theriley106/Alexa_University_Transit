d6e89f153e4ed74976229368a0c355f1cae49813
Theriley106 <christopherlambert106@gmail.com>
2018-03-29 17:17:36 -0400

    Added if name to scrapeStuff.py

--- scrapeStuff.py
+++ scrapeStuff.py
@@ -28,10 +28,11 @@ def getTopResult(companyName):
 		print exp
 		return ""
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-res = requests.get('http://translocrider.com/our-agencies', headers=headers)
-page = bs4.BeautifulSoup(res.text, 'lxml')
-listOfSchools = page.select('#x-section-1 p')[0].getText().split('\n')
+if __name__ == '__main__':
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	res = requests.get('http://translocrider.com/our-agencies', headers=headers)
+	page = bs4.BeautifulSoup(res.text, 'lxml')
+	listOfSchools = page.select('#x-section-1 p')[0].getText().split('\n')
 
-for e in listOfSchools:
-	saveToCSV(e)
+	for e in listOfSchools:
+		saveToCSV(e)
