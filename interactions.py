711dd9b857bcdbd96122e4a3b346cbfdf3778460
Theriley106 <christopherlambert106@gmail.com>
2018-03-29 20:19:59 -0400

    Added url constants

--- interactions.py
+++ interactions.py
@@ -1,1 +1,12 @@
 import requests
+import threading
+
+GET_ALL_ROUTES  = "https://feeds.transloc.com/3/stops?agencies={0}"
+GET_CURRENT_INFO = "https://feeds.transloc.com/3/vehicle_statuses?agencies={0}"
+GET_STOP_INFO = "https://feeds.transloc.com/3/stops?&agencies={0}"
+
+def getRoutes(agencyId):
+
+
+def grabAllInfo(agencyId):
+	res = requests.get("")
