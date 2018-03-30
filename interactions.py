176ae3aca4a7881750a90aa3d0430f29ba1ba429
Theriley106 <christopherlambert106@gmail.com>
2018-03-29 20:34:09 -0400

    Added get announcements function

--- interactions.py
+++ interactions.py
@@ -6,7 +6,7 @@ import json
 GET_ALL_ROUTES  = "https://feeds.transloc.com/3/routes?agencies={0}"
 GET_CURRENT_INFO = "https://feeds.transloc.com/3/vehicle_statuses?agencies={0}"
 GET_STOP_INFO = "https://feeds.transloc.com/3/stops?&agencies={0}"
-
+GET_ANNOUNCEMENTS = "https://feeds.transloc.com/3/announcements?agencies={0}"
 
 def grabAllInfo(agencyId):
 	tempDict = {}
