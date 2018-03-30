db9bef823b8bd07d27cfa5c4b5fb03728d2b2eee
Theriley106 <christopherlambert106@gmail.com>
2018-03-29 20:21:12 -0400

    Added get routes

--- interactions.py
+++ interactions.py
@@ -6,7 +6,8 @@ GET_CURRENT_INFO = "https://feeds.transloc.com/3/vehicle_statuses?agencies={0}"
 GET_STOP_INFO = "https://feeds.transloc.com/3/stops?&agencies={0}"
 
 def getRoutes(agencyId):
-
+	url = GET_ALL_ROUTES.format(agencyId)
+	tempDict["Routes"] = requests.get(url).json()
 
 def grabAllInfo(agencyId):
 	res = requests.get("")
