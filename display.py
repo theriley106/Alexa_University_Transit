9f47f466cabd5d9621c6273ab635d4e2b865aed2
Theriley106 <christopherlambert106@gmail.com>
2018-04-11 01:01:28 -0400

    Added segment to gen map

--- display.py
+++ display.py
@@ -2,7 +2,12 @@
 #https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-a+9ed4bd(-122.46589,37.77343),pin-s-b+000(-122.42816,37.75965)/auto/500x300?access_token=pk.eyJ1IjoidGhlcmlsZXkxMDYiLCJhIjoiY2o4cm1xZzdnMTgzMDMzbnR1d3Q2Y2p6byJ9.C-Kuwbt67fBaEg0V5rGXzg
 
 
-def genMap(point1, point2=(-82.791442, 34.641643)):
-	url = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-a+9ed4bd{},pin-s-b+000{}/auto/1024x600?access_token=pk.eyJ1IjoidGhlcmlsZXkxMDYiLCJhIjoiY2o4cm1xZzdnMTgzMDMzbnR1d3Q2Y2p6byJ9.C-Kuwbt67fBaEg0V5rGXzg".format(str(point1).replace(" ", ""), str(point2).replace(" ", ""))
+def genMap(point1, point2=None, segment=None):
+	url = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-a+9ed4bd{}".format(str(point1).replace(" ", ""))
+	if point2 != None:
+		url = url + ",pin-s-b+000{}".format(str(point2).replace(" ", ""))
+	if segment != None:
+		url = url + "path-5+f44-0.5({})".format(str(segment))
+	url = url + "/auto/1024x600?access_token=pk.eyJ1IjoidGhlcmlsZXkxMDYiLCJhIjoiY2o4cm1xZzdnMTgzMDMzbnR1d3Q2Y2p6byJ9.C-Kuwbt67fBaEg0V5rGXzg"
 	return url
 
